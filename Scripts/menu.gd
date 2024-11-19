d20bec7b1cc0afccbe135c2fff474985f074e1e8
Kaaaamamura <99218981+Kaaaamamura@users.noreply.github.com>
2024-11-19 16:58:29 -0500

    Place holder menú de opciones
    Mejoras visuales al menú de opciones

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,13 +1,22 @@
+class_name MainMenu
 extends Control
+@onready var margin_container: MarginContainer = $MarginContainer
+@onready var options: OptionsMenu = $Options
 
+func _ready() -> void:
+	options.exit_options_menu.connect(_on_exit_options_menu)
 
 func _on_play_pressed() -> void:
 	pass
 
-
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menus/Options.tscn")
+	margin_container.visible= false
+	options.set_process(true)
+	options.visible = true
 
+func _on_exit_options_menu() -> void:
+	margin_container.visible = true
+	options.visible = false
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
